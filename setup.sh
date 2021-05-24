946900e65e55d079bbbc621c1536be601b318c97
Gladiator07 <atharvaaingle@gmail.com>
2021-05-24 05:48:53 +0000

    setup data script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,15 @@
 echo "Installing dependencies"
 pip3 install six
 pip3 install black
+
+cd /content/Text-to-image-synthesis-with-GANs/
+cp -r /content/drive/MyDrive/Project_Data/Text-to-image-AttnGAN/data .
+
+cd data/
+tar -xf CUB_200_2011.tgz
+tar -xf text.tar.xz
+
+rm CUB_200_2011.tgz
+rm text.tar.xz
+
+echo "Workspace setup done ..."
