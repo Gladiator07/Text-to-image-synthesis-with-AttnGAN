4f8aecec0fab09cd028742a67cdd919760e72301
Gladiator07 <atharvaaingle@gmail.com>
2021-05-25 23:05:12 +0530

    update setup for heroku

--- setup.sh
+++ setup.sh
@@ -1,16 +1,8 @@
-# Setup script for Google Cloud VM
-echo "Installing dependencies"
-pip3 install six
-pip3 install black
-
-cd /content/Text-to-image-synthesis-with-GANs/
-cp -r /content/drive/MyDrive/Project_Data/Text-to-image-AttnGAN/data .
-
-cd data/
-tar -xf CUB_200_2011.tgz
-tar -xf text.tar.xz
-
-rm CUB_200_2011.tgz
-rm text.tar.xz
-
-echo "Workspace setup done ..."
+mkdir -p ~/.streamlit/
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
+" > ~/.streamlit/config.toml
