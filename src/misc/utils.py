d9f5f9793467254bf724f99da6dda9231b6f5c1b
Gladiator07 <atharvaaingle@gmail.com>
2021-05-26 00:33:15 +0530

    update font path

--- src/misc/utils.py
+++ src/misc/utils.py
@@ -33,7 +33,7 @@ def drawCaption(convas, captions, ixtoword, vis_size, off1=2, off2=2):
     # get a font
     # fnt = None  # ImageFont.truetype('Pillow/Tests/fonts/FreeMono.ttf', 50)
     # fnt = ImageFont.truetype('Pillow/Tests/fonts/FreeMono.ttf', 50)
-    fnt = ImageFont.truetype(50)
+    fnt = ImageFont.truetype("https://github.com/opensourcedesign/fonts/blob/master/gnu-freefont_freemono/FreeMono.ttf", 50)
     # get a drawing context
     d = ImageDraw.Draw(img_txt)
     sentence_list = []
